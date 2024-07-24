a83a119a35b1a53a7e690543c47f8bab3d6193f6
firatkaanbitmez <firatbitmez@gmail.com>
2024-07-24 17:20:35 +0300

    Gereksiz paket silindi

--- UserManagement/UserManagement.Repository/Migrations/20240724123528_InitialCreate.Designer.cs
+++ UserManagement/UserManagement.Repository/Migrations/20240724123528_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using UserManagement.Repository.Data;
 namespace UserManagement.Repository.Migrations
 {
     [DbContext(typeof(UserManagementDbContext))]
-    [Migration("20240724105726_InitialCreate")]
+    [Migration("20240724123528_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
