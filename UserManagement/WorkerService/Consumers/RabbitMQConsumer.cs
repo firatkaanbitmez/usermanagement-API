43db2799b1d52ae7802e3e7e0851bda18126aedf
firatkaanbitmez <firatbitmez@gmail.com>
2024-07-29 15:25:22 +0300

    WorkerService yapısı düzenlendi. Artık Tüm işlemler Worker.cs dosyasından yapılmıyor. Consumer ve Connection için iki yapı eklendi. Test edildi bir sorun yok

---
+++ UserManagement/WorkerService/Consumers/RabbitMQConsumer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using WorkerService.Infrastructure;
+
+namespace WorkerService.Consumers
+{
+    public class RabbitMQConsumer
+    {
+        private readonly ILogger<RabbitMQConsumer> _logger;
+        private readonly IModel _channel;
+
+        public RabbitMQConsumer(RabbitMQConnection rabbitMQConnection, ILogger<RabbitMQConsumer> logger)
+        {
+            _logger = logger;
+            _channel = rabbitMQConnection.GetChannel();
+        }
+
+        public void StartConsuming()
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                _logger.LogInformation($"Received message: {message}");
+            };
+            _channel.BasicConsume(queue: "UserQueue", autoAck: true, consumer: consumer);
+        }
+    }
+}
---
+++ UserManagement/WorkerService/Infrastructure/RabbitMQConnection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace WorkerService.Infrastructure
+{
+    public class RabbitMQConnection : IDisposable
+    {
+        private readonly ILogger<RabbitMQConnection> _logger;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public RabbitMQConnection(IConfiguration configuration, ILogger<RabbitMQConnection> logger)
+        {
+            _logger = logger;
+            var factory = new ConnectionFactory()
+            {
+                HostName = configuration["RabbitMQ:Host"],
+                VirtualHost = configuration["RabbitMQ:VirtualHost"],
+                UserName = configuration["RabbitMQ:Username"],
+                Password = configuration["RabbitMQ:Password"]
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "UserQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        public IModel GetChannel()
+        {
+            return _channel;
+        }
+
+        public void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+    }
+}
