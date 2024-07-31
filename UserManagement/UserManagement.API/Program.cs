81ecbd6236586ff844fe6d18b02891a91b4ec2fa
firatkaanbitmez <firatbitmez@gmail.com>
2024-07-31 17:51:34 +0300

    middlewaredeki hata çözümlendi.

--- UserManagement/UserManagement.API/Program.cs
+++ UserManagement/UserManagement.API/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddSingleton<RabbitMQService>();
 
 var app = builder.Build();
 
-app.UseMiddleware<UserIsActiveMiddleware>();
+app.UseMiddleware<UserCheckMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
